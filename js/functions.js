91665d09c10f761219a3868b13006939ac99d130
lucasweiby <lucas_weiby@hotmail.com>
2018-04-17 15:40:24 -0300

    Body changes

--- js/functions.js
+++ js/functions.js
@@ -2,11 +2,43 @@ $(function(){
     $(window).scroll(function(){
         var wscroll = $(window).scrollTop();
 
-        if (wscroll > 100) {
+        if (wscroll > $(window).height()) {
             $('#nav').removeClass("nav-hide").addClass("nav-show");
+
         }
         else {
             $('#nav').removeClass("nav-show").addClass("nav-hide");
         }
     });
+});
+
+window.sr = ScrollReveal(); 
+
+sr.reveal('.nav-hide',{
+	duration: 1000,
+	origin: 'top',
+	reset: true,
+	scale: 1
+});
+
+sr.reveal('.nav-show',{
+	reset: true
+});
+
+sr.reveal('.banner-text',{
+	reset: true,
+	duration: 1000
+},500);
+
+sr.reveal('.frase-text',{
+	duration: 1000,
+	origin: 'top',
+	reset: true
+});
+
+sr.reveal('.autor',{
+	duration: 1000,
+	origin: 'bottom',
+	reset: true,
+	delay: 500
 });
